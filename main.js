09fa4559e6534086638222bb2c619bcf840c8fcc
estherxyz <xyyugi@gmail.com>
2025-01-01 23:07:43 +0800

    feat: init angular with electron.

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'dist/app-new/browser/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
