ee523f99c9393cc78643ad599fcaa92e9f899ac9
estherxyz <xyyugi@gmail.com>
2025-01-02 01:34:20 +0800

    feat: add service about request api.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './service/api.service';
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'app-new';
+
+  data: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getData().subscribe((resp) => {
+      this.data = resp;
+      console.log(this.data);
+    });
+  }
 }
---
+++ src/app/service/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+
+  constructor(private http: HttpClient) {}
+
+  getData(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+}
